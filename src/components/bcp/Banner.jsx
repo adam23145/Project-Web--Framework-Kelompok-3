4c4df2bd4e30efea264de93b15453e358c453a0e
Adam dwi Julianto <81203366+adam23145@users.noreply.github.com>
2022-03-17 20:56:04 +0700

    update

--- src/components/bcp/Banner.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import adam from '../images/people.png';
-
-const Banner = (props) =>{
-    return(
-        <img src={adam} alt=''></img>
-    );
-};
-export default Banner;
