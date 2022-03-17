c959258f8bf7ed60ca7c7dfd02877e9e3de00b63
hyuanindshee <hyuanind@gmail.com>
2022-03-17 21:14:15 +0700

    File react component Input Pass dan Email

---
+++ src/components/LoginRIght/InputEmail.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const InputEmail = (props) => {
+  return (
+    <div>
+      <label for="" className="f-12 font2">
+        Email
+      </label>
+      <input type="text" placeholder="example@gmail.com"></input>
+    </div>
+  );
+};
+export default InputEmail;
---
+++ src/components/LoginRIght/InputPass.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const InputPass = (props) => {
+  return (
+    <div>
+      <label for="" className="f-12 font2">
+        Password
+      </label>
+      <input type="password" placeholder="password"></input>
+    </div>
+  );
+};
+export default InputPass;
