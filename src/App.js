6588962cbd78600cd6bf699506f400a37eb3ffb5
Alvin Pradana Antony <quickgaming321@gmail.com>
2022-03-17 13:25:38 +0700

    Fix React Component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-
-import React,{ Component } from 'react';
-import Header from './components/Header';
-import Body from './components/body';
-
+import React, { Component } from 'react';
+// import Header from './components/Header';
+// import Body from './components/body';
+import Login from './components/Login';
 
 class App extends Component{
   render(){
     return(
-      <div className="App">
-        <Header/>
-        <Body/>
+      <div>
+        <Login/>
       </div>
     );
   }
