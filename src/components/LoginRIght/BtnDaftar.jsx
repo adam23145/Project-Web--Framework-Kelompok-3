f7b8ea5ab28c9e5f1b68fa0d4562488e1570d579
Vivi Handayani <vivihandayaniprob@gmail.com>
2022-03-17 21:20:50 +0700

    Update File React Component Button Daftar Login

---
+++ src/components/LoginRIght/BtnDaftar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const BtnDaftar = (props) => {
+  return (
+    <div className="daftar1">
+      <button>Daftar</button>
+    </div>
+  );
+};
+export default BtnDaftar;
---
+++ src/components/LoginRIght/BtnLogin.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const BtnLogin = (props) => {
+  return (
+    <div className="login1">
+      <button className="font2">Login</button>
+    </div>
+  );
+};
+export default BtnLogin;
