f93348da5388c40adea0f4a3fc010fa81b93b385
Adam dwi Julianto <81203366+adam23145@users.noreply.github.com>
2022-04-12 12:00:29 +0700

    update login

--- progress-projek/src/components/Login.jsx
+++ progress-projek/src/components/Login.jsx
@@ -1,78 +1,91 @@
 import React, { Component, useState } from "react";
 import "../components/css/Login.css";
 import logo from "../components/assets/imgLogin/logo.png";
-import { Link } from "react-router-dom";
+import { Link,Route } from "react-router-dom";
+import { Navigate } from "react";
+import Dashboard from "./Dashboard";
+import { BrowserRouter } from "react-router-dom";
 
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [variabel] = useState("");
 
-function Login(){
-  const [email,setEmail]=useState("");
-  const [password,setPassword]=useState("");
-  const handleproses = () =>{
-    if(!email){
-      alert('please');
-      return;
-    }
-    if(!password){
-      alert('please2');
-      return;
-    }
-  }
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    // call api
+    await fetch(`http://localhost:3001/daftarSiswa?email=${email}&password=${password}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "credentials": "include",
+      },
+    }).then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data == variabel) {
+          alert("Anda Salah Memasukkan");
+      }else{
+          alert("Anda Benar Memasukkan");
+          window.location = "http://localhost:3000/#/dashboard";
+      }
+      });
+  };
   
-  
-    return (
-      <div className="bodyLogin">
-        <div className="formLogin">
-          <div class="row">
-            <div class="columnSide color1 banner">
-              <div className="text-center Title2">
-                <h2 className="font1 f-24">ScolLine.id</h2>
-                <p className="f-14">Belajar dengan nyaman dimana pun dan kapanpun</p>
-              </div>
+
+  return (
+    <div className="bodyLogin">
+      <div className="formLogin">
+        <div class="row">
+          <div class="columnSide color1 banner">
+            <div className="text-center Title2">
+              <h2 className="font1 f-24">ScolLine.id</h2>
+              <p className="f-14">Belajar dengan nyaman dimana pun dan kapanpun</p>
             </div>
-            <div class="columnSide color2">
-              <div className="">
-                <div class="login">
-                  <img src={logo} alt="" width={95} className="LogoLogin" />
-                  <form action="" id="formLogin">
-                    <h1 className="font1">Login</h1>;
-                    <div>
-                      <label for="" className="f-12 font2">
-                        Email
-                      </label>
-                      <input type="text" 
-                      onChange={(e)=>setEmail(e.target.value)}
+          </div>
+          <div class="columnSide color2">
+            <div className="">
+              <div class="login">
+                <img src={logo} alt="" width={95} className="LogoLogin" />
+                <form action="" id="formLogin" onSubmit={handleLogin}>
+                  <h1 className="font1">Login</h1>;
+                  <div>
+                    <label for="" className="f-12 font2">
+                      Email
+                    </label>
+                    <input type="text"
+                      onChange={(e) => setEmail(e.target.value)}
                       placeholder="example@gmail.com"></input>
-                    </div>
-                    <div>
-                      <label for="" className="f-12 font2">
-                        Password
-                      </label>
-                      <input type="password" 
-                      onChange={(e)=>setPassword(e.target.value)} 
+                  </div>
+                  <div>
+                    <label for="" className="f-12 font2">
+                      Password
+                    </label>
+                    <input type="password"
+                      onChange={(e) => setPassword(e.target.value)}
                       placeholder="password"></input>
-                    </div>
+                  </div>
 
-                   
-                    <div className="login1">
-                    <Link to="/dashboard">
-                        <button onClick={handleproses} className="font2">Login</button>
-                        </Link>
-                    </div>
-                    
-                    <div className="daftar1">
+
+                  <div className="login1">
+                      <button type="submit" className="font2">Login</button>
+                  </div>
+
+                  <div className="daftar1">
                     <Link to="/register">
                       <button>Daftar</button>
                     </Link>
-                    </div>
-                  </form>
-                </div>
+                  </div>
+                </form>
               </div>
             </div>
           </div>
         </div>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
 
 export default Login;
